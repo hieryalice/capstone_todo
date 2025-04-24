6cd5a62eb343998e9ecc8d77816fd44813e40f9b
Seungmin Hwang <seungminhwang777@gmail.com>
2025-04-24 20:37:40 +0900

    New

---
+++ todo-back/config.py
@@ -0,0 +1,9 @@
+class Config:
+    MYSQL_HOST = 'localhost'
+    MYSQL_USER = 'todo_user'
+    MYSQL_PASSWORD = '7153'
+    MYSQL_DB = 'todo_db'
+    SECRET_KEY = 'super-secret'
+    
+
+print("✅ config.py loaded", flush=True)
